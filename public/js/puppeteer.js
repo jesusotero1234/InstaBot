4cfa04485730edb69fc35a1f5fd387d7d85ece97
Daniel <danimc28@gmail.com>
2019-11-30 15:52:21 +0100

    DB integration

--- public/js/puppeteer.js
+++ public/js/puppeteer.js
@@ -1,12 +1,12 @@
-const {instagram} = require('./app.js');
+const { instagram } = require('./app.js');
 
 
-(async ()=>{
+(async () => {
 
-    await instagram.initialize() 
-    await instagram.botcreation()
-    await instagram.searchUsername2('therock')
-    await instagram.followUsers('therock')
-    // await instagram.login('robocop20190','147369')
-    
-    })()
+	await instagram.initialize()
+	await instagram.login('robocop20190','147369')
+	// await instagram.botcreation()
+	// await instagram.searchUsername2('therock')
+	await instagram.followUsers('therock')
+
+})()
